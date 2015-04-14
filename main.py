10e67da3432d7a32494e06650f8f0d860e8a8377
drop-root <sagoyanfisic1@gmail.com>
2015-04-14 17:46:59 -0500

    actualizado

--- main.py
+++ main.py
@@ -1,21 +1,20 @@
 import os
-import urllib
-import webapp2
-import jinja2
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp import util
+from google.appengine.ext.webapp import template
 
+class MainHandler(webapp.RequestHandler):
+  def get (self, q):
+    if q is None:
+      q = 'index.html'
 
-JINJA_ENVIRONMENT = jinja2.Environment(
-    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
-    extensions=['jinja2.ext.autoescape'],
-    autoescape=True)
-# [END imports]
+    path = os.path.join (os.path.dirname (__file__), q)
+    self.response.headers ['Content-Type'] = 'text/html'
+    self.response.out.write (template.render (path, {}))
 
-class MainHandler(webapp2.RequestHandler):
-    def get(self):
-      template = JINJA_ENVIRONMENT.get_template('index.html')
-      self.response.headers.add_header("Access-Control-Allow-Origin", "*")
-      self.response.write(template.render())
-        
-app = webapp2.WSGIApplication([
-    ('/', MainHandler)
-], debug=True)
+def main ():
+  application = webapp.WSGIApplication ([('/(.*html)?', MainHandler)], debug=True)
+  util.run_wsgi_app (application)
+
+if __name__ == '__main__':
+  main ()
